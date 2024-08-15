df83e8e45bda28bcd9fa48c989e119c89aeac527
Mikeps2 <mikeps.dev@gmail.com>
2024-08-15 17:35:53 -0300

    pastas

---
+++ 2023/E. Condicional/main.py
@@ -0,0 +1,20 @@
+#Caio, Nifane, Micael
+import math
+
+idade = float(input("Digite a idade do frango (d): "))
+pesoAnimal = float(input("Digite o peso vivo do frango (g): "))
+
+pesoVivo = 20+200/(0.1+4*math.exp(1-0.4*idade))
+    
+if pesoAnimal < pesoVivo*0.8:
+    supA = 10*math.log(5+idade)+10
+    print("Peso do frango abaixo do esperado, "
+    f"{supA:.2f}g do suplemento A devem ser ministrados")
+    
+elif pesoAnimal > pesoVivo*1.2:
+    supB = 0.45*idade+5
+    print("Peso do frango acima do esperado, "
+    f"{supB:.2f}g do suplemento B devem ser ministrados")
+    
+else:
+    print("O frango esta saudavel!")
---
+++ 2023/E. Sequencial/main.py
@@ -0,0 +1,17 @@
+#Trio: Micael Pereira, Mateus Martins, Herisson Olimpio
+
+condTer = float(input("Digite o valor da condutividade termica (W/(m.K)): "))
+tempInt = float(input("Digite o valor da temperatura interna (°C): "))
+tempBul = float(input("Digite o valor da temperatura de buldo seco (°C): "))
+umidRel = float(input("Digite o valor da umidade relativa (%): "))
+convInt = float(input("Digite o valor da convecção interna (W/(m².°C)): "))
+convExt = float(input("Digite o valor da convecção externa (W/(m².°C)): "))
+
+tempOrv =  (109.8 + tempBul) * (umidRel / 100)**0.1247 -109.8
+C1 = (tempOrv - tempInt) / (tempBul - tempOrv)
+espeIso = condTer * (C1*(1/convInt) - (1/convExt)) * 1000
+
+#print(f"tempOrv: {tempOrv:.2f}")
+#print(f"C1: {C1:.2f}")
+
+print(f"Espessura do isolamento: {espeIso:.2f}", "mm")
