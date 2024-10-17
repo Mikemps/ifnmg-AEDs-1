0b4ecba17e54943003c015bcdffcb00cc913b611
Mikeps2 <mikeps.dev@gmail.com>
2024-10-17 09:53:03 -0300

    1°P 2024

--- 2024/(T-03) rep2.c/main.c
+++ 2024/(T-03) rep2.c/main.c
@@ -17,30 +17,30 @@ int main()
  
     for (int estacoes=1; estacoes<=estMonit; estacoes++){
         do{
-            printf("\n------------ %i� Esta��o ------------", estacoes);
+            printf("\n------------ %i° Estação ------------", estacoes);
             printf("\nInforme o valor de PM2.5 (�g/m�): ");
             scanf("%f", &pm25);
             printf("Informe o valor de CO2 (ppm): ");
             scanf("%f", &co2);
 
             if (pm25==0 || co2==0)
-                printf("\nZero n�o � aceito como valor de entrada.\nInforme novamente!!\n");
+                printf("\nZero não é aceito como valor de entrada.\nInforme novamente!!\n");
         } while (pm25==0 || co2==0);
 
         if (pm25 < 12 && co2 < 400){
-            printf("Situa��o da esta��o: Boa!!\n");
+            printf("Situação da estação: Boa!!\n");
             boa++;
             totalPMBoa += pm25;
             totalCO2Boa += co2;
         }
         else if (pm25 >= 35 || co2 >= 800){
-            printf("Situa��o da esta��o: Ruim.\n");
+            printf("Situação da estação: Ruim.\n");
             ruim++;
             totalPMRuim += pm25;
             totalCO2Ruim += co2;
         }
         else{
-            printf("Situa��o da esta��o: Moderada!\n");
+            printf("Situação da estação: Moderada!\n");
             moderada++;
             totalPMMode += pm25;
             totalCO2Mode += co2;
@@ -48,17 +48,17 @@ int main()
     }
     printf("\n------------ Resultados ------------");
     if (boa > 0) {
-        printf("\nEstacoes Boas: %d\n", boa);
+        printf("\nEstações Boas: %d\n", boa);
         printf("Media PM2.5 (Boa): %.2f �g/m�\n", totalPMBoa / boa);
         printf("Media CO2 (Boa): %.2f ppm\n", totalCO2Boa / boa);
     }
     if (moderada > 0) {
-        printf("\nEstacoes Moderadas: %d\n", moderada);
+        printf("\nEstações Moderadas: %d\n", moderada);
         printf("Media PM2.5 (Moderada): %.2f �g/m�\n", totalPMMode / moderada);
         printf("Media CO2 (Moderada): %.2f ppm\n", totalCO2Mode / moderada);
     }
     if (ruim > 0) {
-        printf("\nEstacoes Ruins: %d\n", ruim);
+        printf("\nEstações Ruins: %d\n", ruim);
         printf("Media PM2.5 (Ruim): %.2f �g/m�\n", totalPMRuim / ruim);
         printf("Media CO2 (Ruim): %.2f ppm\n", totalCO2Ruim / ruim);
     }
